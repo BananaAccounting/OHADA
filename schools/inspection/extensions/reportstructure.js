a9a04f174c5c63db091e8e7518ac55a7936f574b
Joel Mbiye <joembiye@gmail.com>
2025-03-09 06:53:16 +0100

    add financial report IPP secondary pool

--- schools/inspection/extensions/reportstructure.js
+++ schools/inspection/extensions/reportstructure.js
@@ -27,7 +27,7 @@
  */
 
 
-// Balance sheet 
+// Report structure for the financial report
 function createReportStructureFinancialReport() {
     var reportStructure = [];
 
@@ -111,6 +111,59 @@ function createReportStructureFinancialReport() {
     reportStructure.push({"id":"N", "type":"total", "description":"SOLDE HORS SESSION EXETAT = (REX3 - (Somme de DEX311 à DEX320)", "sum":"REX3;-H"});
     reportStructure.push({"id":"O", "type":"total", "description":"SOLDE SESSION EXETAT = (REX4 - (Somme de DEX410 à DEX421)", "sum":"REX4;-I"});
     reportStructure.push({"id":"P", "type":"total", "description":"SOLDE GENERAL = (D-J)", "sum":"D;-J"});
+
+
+    return reportStructure;
+}
+
+// 
+function createReportStructureFinancialReportSecondaryPool() {
+    var reportStructure = [];
+
+    reportStructure.push({"id":"SI", "type":"group", "bclass":"4", "description":"Solde Initial"});
+    reportStructure.push({"id":"RFF1", "type":"group", "bclass":"4", "description":"Frais Fonctionnement Appuis Parents"});
+    reportStructure.push({"id":"RFF2", "type":"group", "bclass":"4", "description":"Frais Fonctionnement Trésor public"});
+    reportStructure.push({"id":"A", "type":"total", "description":"Total frais Fonctionnement (RFF1+RFF2)", "sum":"RFF1;RFF2"});
+    reportStructure.push({"id":"TENASOSP", "type":"group", "bclass":"4", "description":"Participation Candidat TENASOSP"});
+    reportStructure.push({"id":"B", "type":"total", "description":"Total TENASOSP", "sum":"A;TENASOSP"});
+    reportStructure.push({"id":"C", "type":"total", "description":"TOTAL GENERAL RECETTES (SI+A+B)", "sum":"SI;A;B"});
+
+    reportStructure.push({"id":"DFF1", "type":"group", "bclass":"3", "description":"FOURNITURES DES BUREAUX"});
+    reportStructure.push({"id":"DFF2", "type":"group", "bclass":"3", "description":"INTRANTS INFORMATIQUES"});
+    reportStructure.push({"id":"DFF3", "type":"group", "bclass":"3", "description":"FORFAIT INTERNET"});
+    reportStructure.push({"id":"DFF4", "type":"group", "bclass":"3", "description":"COMMUNICATION/INFORMATION"});
+    reportStructure.push({"id":"DFF5", "type":"group", "bclass":"3", "description":"FORMULE D'EXPLOITATION"});
+    reportStructure.push({"id":"DFF6", "type":"group", "bclass":"3", "description":"FORMATION"});
+    reportStructure.push({"id":"DFF7", "type":"group", "bclass":"3", "description":"GESTION VEHICULES"});
+    reportStructure.push({"id":"DFF8", "type":"group", "bclass":"3", "description":"GESTION MATERIEL/INFORMATIQUE"});
+    reportStructure.push({"id":"DFF9", "type":"group", "bclass":"3", "description":"RENOUVELEMENT MOBILIERS"});
+    reportStructure.push({"id":"DFF10", "type":"group", "bclass":"3", "description":"INTERVENTIONS SOCIALES"});
+    reportStructure.push({"id":"DFF11", "type":"group", "bclass":"3", "description":"MISSION DE SERVICE"});
+    reportStructure.push({"id":"DFF12", "type":"group", "bclass":"3", "description":"RAFFRAICHISSEMENT BUREAUX"});
+    reportStructure.push({"id":"DFF13", "type":"group", "bclass":"3", "description":"RENCONTRES LOCALES"});
+    reportStructure.push({"id":"DFF14", "type":"group", "bclass":"3", "description":"DIVERS"});
+    reportStructure.push({"id":"D", "type":"total", "description":"TOTAL DEPENSES F F (Somme de DFF1 à DFF14)", "sum":"DFF1;DFF2;DFF3;DFF4;DFF5;DFF6;DFF7;DFF8;DFF9;DFF10;DFF11;DFF12;DFF13;DFF14"});
+    
+    reportStructure.push({"id":"DTENA01", "type":"group", "bclass":"3", "description":"Appui à la hiérarchie"});
+    reportStructure.push({"id":"DTENA02", "type":"group", "bclass":"3", "description":"Versement à l'échelon national"});
+    reportStructure.push({"id":"DTENA03", "type":"group", "bclass":"3", "description":"Versement à l'échelon provincial"});
+    reportStructure.push({"id":"DTENA04", "type":"group", "bclass":"3", "description":"Appui à l'ENAFEP"});
+    reportStructure.push({"id":"DTENA05", "type":"group", "bclass":"3", "description":"Rétrocession aux écoles"});
+    reportStructure.push({"id":"DTENA06", "type":"group", "bclass":"3", "description":"Appui psycho-technique"});
+    reportStructure.push({"id":"DTENA07", "type":"group", "bclass":"3", "description":"Distribution des colis dans les centres"});
+    reportStructure.push({"id":"DTENA08", "type":"group", "bclass":"3", "description":"Recouvrement"});
+    reportStructure.push({"id":"DTENA09", "type":"group", "bclass":"3", "description":"Centre d'administration"});
+    reportStructure.push({"id":"DTENA10", "type":"group", "bclass":"3", "description":"Permanence"});
+    reportStructure.push({"id":"DTENA11", "type":"group", "bclass":"3", "description":"Supervision"});
+    reportStructure.push({"id":"DTENA12", "type":"group", "bclass":"3", "description":"Dépouillement et codification"});
+    reportStructure.push({"id":"DTENA13", "type":"group", "bclass":"3", "description":"Planification de la correction"});
+    reportStructure.push({"id":"DTENA14", "type":"group", "bclass":"3", "description":"Correction"});
+    reportStructure.push({"id":"E", "type":"total", "description":"TOTAL DEPENSES TENASOSP = (Somme de DTENA01 à DTENA14)", "sum":"DTENA01;DTENA02;DTENA03;DTENA04;DTENA05;DTENA06;DTENA07;DTENA08;DTENA09;DTENA10;DTENA11;DTENA12;DTENA13;DTENA14"});
+
+    reportStructure.push({"id":"F", "type":"total", "description":"TOTAL GENERAL DEPENSES = (D+E)", "sum":"D;E"});
+    reportStructure.push({"id":"G", "type":"total", "description":"SOLDE FRAIS DE FONCTIONNEMENT = (RFF1+RFF2) - (Somme de DFF1 à DFF14)", "sum":"A;-D"});
+    reportStructure.push({"id":"H", "type":"total", "description":"SOLDE TENASOSP = (RTENA) - (Somme de DTENA01 à DTENA14)", "sum":"B;-E"});
+    reportStructure.push({"id":"I", "type":"total", "description":"SOLDE GENERAL = C-F", "sum":"C;-F"});
 
 
     return reportStructure;
